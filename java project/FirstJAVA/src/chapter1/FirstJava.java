d713287ac0da07aef845a0e805cee2c6fdd407d1
dkfmfm <phj9516@gmail.com>
2020-04-09 09:47:32 +0900

    0408 study

---
+++ java project/FirstJAVA/src/chapter1/FirstJava.java
@@ -0,0 +1,16 @@
+package chapter1;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		
+		
+		int num;
+		num = 10+20;
+		if ( num > 10 )
+			System.out.println("범위를 초과했습니다");
+		System.out.println(num);
+		
+	}
+
+}
