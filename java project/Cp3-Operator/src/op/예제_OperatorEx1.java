7b24945d8f3b0dbfa38696092789b0730ce01d07
dkfmfm <phj9516@gmail.com>
2020-04-10 18:21:09 +0900

    200410 study
    200410 study

---
+++ java project/Cp3-Operator/src/op/예제_OperatorEx1.java
@@ -0,0 +1,23 @@
+package op;
+
+public class 예제_OperatorEx1 {
+	public static void main(String args[]) {
+//		int i=5;
+//		i++;
+//		 // i=i+1;과 같은 의미이다. ++i;로 바꿔 써도 결과는 같다.
+//		System.out.println(i);
+//		i=5;
+//		 // 결과를 비교하기 위해 i값을 다시 5로 변경.
+//		++i;
+//		System.out.println(i);
+//		
+		
+		//아래의 코드는 사과를 담는데 필요한 바구니(버켓)의 수를 구하는 코드이다.
+		//만일 사과의수가 123개이고 하나의 바구니에는10개의 사과를 담을 수 있다면,13개의 바구니가 필요할 것이다.(1)에 알맞은 코드를 넣으시오.
+		int a = 123;
+		int b = 10;
+		int c = a/b;
+		System.out.println(c);
+	
+	}
+}
