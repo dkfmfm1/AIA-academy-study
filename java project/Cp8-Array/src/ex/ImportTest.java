4272d9e7f5d661eadfe145a212016fcc5a851aa7
dkfmfm <phj9516@gmail.com>
2020-04-21 18:19:27 +0900

    200421 study

---
+++ java project/Cp8-Array/src/ex/ImportTest.java
@@ -0,0 +1,21 @@
+package ex;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ImportTest {
+
+	public static void main(String[] args) {
+		
+		// java.util.Date 
+		// 날짜와 시간 정보를 하나로 표현
+		Date today = new Date(); 
+		
+		System.out.println(today); //today.toString()
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd. a HH:mm:ss");
+		
+		System.out.println(dateFormat.format(today));
+	}
+
+}
---
+++ java project/Cp9-Modifier/src/ex/Member.java
@@ -0,0 +1,28 @@
+package ex;
+
+public class Member {
+
+	private String name;
+	private String id="dkfmfm1";
+	
+	
+	//  getter / setter 메서드
+	
+	public Member() {
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+
+}
