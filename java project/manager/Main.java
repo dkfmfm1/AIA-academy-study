ef1bd01104f4b4e544542d9039a367491dbd1a45
dkfmfm1 <phj9516@gmail.com>
2020-06-01 16:20:33 +0900

    200601 study

---
+++ java project/manager/Main.java
@@ -0,0 +1,32 @@
+package manager;
+
+import java.util.Scanner;
+
+public class Main {
+
+static Scanner sc = new Scanner(System.in);
+static EMPManager emp = new EMPManager();
+static DEPTManager dept = new DEPTManager();
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("Select Menu");
+		System.out.println("----------------");
+		System.out.println("1. EMP,  2. DEPT");
+		System.out.println("----------------");
+		
+		int select = sc.nextInt();
+		
+		switch (select) {
+		case 1:
+			emp.empManager();
+			break;
+		case 2:
+			dept.deptManager1();
+			break;
+		}
+
+	}
+	
+}
